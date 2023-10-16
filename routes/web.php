8e909840b501df5127d22695c15925214304aa7f
shyamkumbhar1 <shyamkumbhar509@gmail.com>
2023-10-16 14:05:58 +0530

    add test route

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+Route::get('/test', function () {
+    return "test";
+});
 
 require __DIR__.'/auth.php';

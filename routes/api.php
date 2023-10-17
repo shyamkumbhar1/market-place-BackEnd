6012b22c464c7b8146cea3d843a0aa349ad7d71b
shyamkumbhar1 <shyamkumbhar509@gmail.com>
2023-10-17 12:10:02 +0530

    setup

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,32 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+route::get('test',function (){
+    return User::all();
+});
+
+route::post('test',function (Request $request){
+    $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+        'password' => ['required', 'confirmed'],
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    return $user;
 });
